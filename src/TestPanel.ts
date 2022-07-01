1751589418739c60c44df8107a0c1cc8baecf78e
Ali Jaya Meilio <alijayameilio@gmail.com>
2022-07-01 21:35:31 +0700

    refactor <3

--- src/TestPanel.ts
+++ src/TestPanel.ts
@@ -1,27 +1,34 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs/promises";
 
-function getWebviewOptions(extensionContext: vscode.ExtensionContext): vscode.WebviewOptions {
-  var localResourceRoots = [vscode.Uri.joinPath(extensionContext.extensionUri, 'media')];
-  var workspaceFolders = vscode.workspace.workspaceFolders?.map(v => v.uri);
+function getWebviewOptions(
+  extensionContext: vscode.ExtensionContext
+): vscode.WebviewOptions {
+  var localResourceRoots = [
+    vscode.Uri.joinPath(extensionContext.extensionUri, "media"),
+  ];
+  var workspaceFolders = vscode.workspace.workspaceFolders?.map((v) => v.uri);
   if (workspaceFolders) {
     localResourceRoots = localResourceRoots.concat(workspaceFolders);
   }
 
   return {
     enableScripts: true,
-    localResourceRoots
+    localResourceRoots,
   };
 }
 
 export default class TestPanel {
   public static currentPanel: TestPanel | undefined;
 
-  public static readonly viewType = 'testPanel';
+  public static readonly viewType = "testPanel";
 
   private readonly _panel: vscode.WebviewPanel;
   private readonly _webview: vscode.Webview;
   private readonly _extensionContext: vscode.ExtensionContext;
   private readonly _extensionUri: vscode.Uri;
+
+  private _cacheHTML: string | undefined;
 
   public static createOrShow(extensionContext: vscode.ExtensionContext) {
     const column = vscode.window.activeTextEditor?.viewColumn;
@@ -35,19 +42,25 @@ export default class TestPanel {
     // Otherwise, create a new panle.
     const panel = vscode.window.createWebviewPanel(
       this.viewType,
-      'Test Panel',
+      "Test Panel",
       column ?? vscode.ViewColumn.One,
-      getWebviewOptions(extensionContext),
+      getWebviewOptions(extensionContext)
     );
 
     this.currentPanel = new TestPanel(panel, extensionContext);
   }
 
-  public static revive(panel: vscode.WebviewPanel, extensionContext: vscode.ExtensionContext) {
+  public static revive(
+    panel: vscode.WebviewPanel,
+    extensionContext: vscode.ExtensionContext
+  ) {
     this.currentPanel = new TestPanel(panel, extensionContext);
   }
 
-  private constructor(panel: vscode.WebviewPanel, extensionContext: vscode.ExtensionContext) {
+  private constructor(
+    panel: vscode.WebviewPanel,
+    extensionContext: vscode.ExtensionContext
+  ) {
     this._panel = panel;
     this._webview = panel.webview;
     this._extensionContext = extensionContext;
@@ -55,15 +68,21 @@ export default class TestPanel {
 
     this._update();
 
-    this._panel.onDidDispose(() => this.dispose(), null, extensionContext.subscriptions);
-  
-    this._panel.onDidChangeViewState( e => {
-      if (this._panel.visible) {
-        this._update();
-      }
-    },
-    null,
-    extensionContext.subscriptions);
+    this._panel.onDidDispose(
+      () => this.dispose(),
+      null,
+      extensionContext.subscriptions
+    );
+
+    this._panel.onDidChangeViewState(
+      (e) => {
+        if (this._panel.visible) {
+          this._update();
+        }
+      },
+      null,
+      extensionContext.subscriptions
+    );
   }
 
   public dispose() {
@@ -72,39 +91,53 @@ export default class TestPanel {
     this._panel.dispose();
   }
 
-  private _update() {
+  private async _update() {
     const panel = this._panel;
     const webview = this._panel.webview;
-    
+
     panel.title = "Hello World";
-    webview.html = this._getHtmlForWebview(webview);
+    webview.html = await this._getHtmlForWebview(webview);
   }
 
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-    return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test Panel</title>
-      </head>
-      <body>
-        <h1>Add Image</h1>
-        <input id="pathInput" />
-        <button id="addButton">Add Image</button>
-        <div id="imageList"></div>
-        <script src="${scriptUri}"></script>
-      </body>
-      </html>
-    `;
+  private async _getHtmlForWebview(webview: vscode.Webview) {
+    if (!this._cacheHTML) {
+      const htmlUri = vscode.Uri.joinPath(
+        this._extensionUri,
+        "media",
+        "index.html"
+      );
+      let html = await fs.readFile(htmlUri.fsPath, "utf8");
+      const baseUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(this._extensionUri, "/")
+      );
+      html = html.replace("${baseUri}", baseUri.toString());
+      this._cacheHTML = html;
+    }
+    return this._cacheHTML;
+    // return `
+    //   <!DOCTYPE html>
+    //   <html lang="en">
+    //   <head>
+    //     <base href="${baseUri}">
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <title>Test Panel</title>
+    //   </head>
+    //   <body>
+    //     <h1>Add Image</h1>
+    //     <input id="pathInput" />
+    //     <button id="addButton">Add Image</button>
+    //     <div id="imageList"></div>
+    //     <script src="media/main.js"></script>
+    //   </body>
+    //   </html>
+    // `;
   }
 
   public loadImage(uri: vscode.Uri) {
     this._webview.postMessage({
       command: "loadImage",
-      path: this._webview.asWebviewUri(uri).toString()
+      path: this._webview.asWebviewUri(uri).toString(),
     });
   }
 }
